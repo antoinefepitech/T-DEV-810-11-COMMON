c312df9c8752762f162c7dc96df4737122676e2f
mdufour <mathieu.dufour@whereyoulove.com>
2020-03-09 15:26:13 +0100

    done entrainement with vgg16, 0.7 accuracy

--- mdufour_ia/main.py
+++ mdufour_ia/main.py
@@ -22,23 +22,24 @@ VAL_DATA_PATH = 'chest_xray/val'
 CLASS_NAMES = ['NORMAL', 'PNEUMONIA']
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 VERBOSE = 1
+MODEL_NAME = 'vgg16'
 
 METRICS = [
   tf.keras.metrics.BinaryAccuracy(name='accuracy', dtype=tf.float32),
-  tf.keras.metrics.TruePositives(name='true_positives', dtype=tf.float32),
-  tf.keras.metrics.FalsePositives(name='false_positives', dtype=tf.float32),
-  tf.keras.metrics.TrueNegatives(name='true_negatives', dtype=tf.float32),
-  tf.keras.metrics.FalseNegatives(name='false_negatives', dtype=tf.float32), 
-  tf.keras.metrics.Precision(name='precision', dtype=tf.float32),
-  tf.keras.metrics.Recall(name='recall', dtype=tf.float32),
-  tf.keras.metrics.AUC(name='auc', dtype=tf.float32),
+  # tf.keras.metrics.TruePositives(name='true_positives', dtype=tf.float32),
+  # tf.keras.metrics.FalsePositives(name='false_positives', dtype=tf.float32),
+  # tf.keras.metrics.TrueNegatives(name='true_negatives', dtype=tf.float32),
+  # tf.keras.metrics.FalseNegatives(name='false_negatives', dtype=tf.float32), 
+  # tf.keras.metrics.Precision(name='precision', dtype=tf.float32),
+  # tf.keras.metrics.Recall(name='recall', dtype=tf.float32),
+  # tf.keras.metrics.AUC(name='auc', dtype=tf.float32),
 ]
 
 def save_model(model=None, model_name='vgg16'):
   """
   Save a TF Model into h5 format
   """
-  model.save('saved_model/{}/model.h5'.format(model_name))
+  model.save('saved_model/{}/model.h5'.format(MODEL_NAME))
   print("Model saved successfully.")
 
 def get_callbacks(model_size='small'):
@@ -84,6 +85,25 @@ def main():
   """
   Main function
   """
+  modelLoaded = False
+  model = None
+  # if the model already exist, load it.
+  if (os.path.isfile('saved_model/{}/model.h5'.format(MODEL_NAME))):
+    model = tf.keras.models.load_model('saved_model/{}/model.h5'.format(MODEL_NAME))
+    modelLoaded = True
+    print('Model successfully loaded.')
+  else:
+    # Get the model
+    model = get_model(
+      model='vgg',
+      nodes=16,
+      optimizer='adam',
+      loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+      hidden_activation='relu',
+      final_activation='softmax',
+      metrics=METRICS
+    )
+
   # To get the nb of steps and how many images we got
   nb_normal_tr = len(os.listdir('{}/NORMAL'.format(TRAIN_DATA_PATH)))
   nb_pneumonia_tr = len(os.listdir('{}/PNEUMONIA'.format(TRAIN_DATA_PATH)))
@@ -114,42 +134,31 @@ def main():
     class_mode='binary'
   )
 
-  # Get the model
-  model = get_model(
-    model='vgg',
-    nodes=16,
-    optimizer='adam',
-    loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-    hidden_activation='relu',
-    final_activation='softmax',
-    metrics=None
-  )
+  if not modelLoaded:
+    # Train the model
+    model.fit(
+      train_data_gen,
+      callbacks=get_callbacks(),
+      steps_per_epoch=total_train // BATCH_SIZE,
+      epochs=NB_EPOCHS,
+      validation_data=val_data_gen,
+      validation_steps=total_val // BATCH_SIZE
+    )
+    save_model(model, 'vgg16')
 
-  # Train the model
-  model.fit(
-    train_data_gen,
-    callbacks=get_callbacks(),
-    steps_per_epoch=total_train // BATCH_SIZE,
-    epochs=NB_EPOCHS,
-    validation_data=val_data_gen,
-    validation_steps=total_val // BATCH_SIZE
-  )
-
-  model.summary()
-
-  save_model(model, 'vgg16')
+    model.summary()
 
   # Use a testing model to display metrics
-  testing_model = keras.Sequential([model, keras.layers.Softmax()])
-  testing_model.compile(
-    loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
-    optimizer='adam',
-    metrics=METRICS
-  )
+  # testing_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+  # testing_model.compile(
+  #   loss=tf.keras.losses.BinaryCrossentropy(from_logits=False),
+  #   optimizer='adam',
+  #   metrics=METRICS
+  # )
 
   # Display metrics for testing purpose
   print('Normal/Pneumonia vgg trained model : ')
-  results = testing_model.evaluate(test_data_gen)
+  results = model.evaluate(test_data_gen)
   for name, value in zip(model.metrics_names, results):
     print(f'{name} : {value}')
 
